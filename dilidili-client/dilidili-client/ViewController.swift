64fa337a65f9ce5962350f88358b5a3608a65973
Ke哀 <604922471@qq.com>
2018-04-24 17:49:19 +0800

    init

---
+++ dilidili-client/dilidili-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dilidili-client
+//
+//  Created by YYInc on 2018/4/24.
+//  Copyright © 2018年 caoxuerui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dilidili-client/dilidili-clientTests/dilidili_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  dilidili_clientTests.swift
+//  dilidili-clientTests
+//
+//  Created by YYInc on 2018/4/24.
+//  Copyright © 2018年 caoxuerui. All rights reserved.
+//
+
+import XCTest
+@testable import dilidili_client
+
+class dilidili_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
